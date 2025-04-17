9f7041e59e8f89cb6ccfbaf4944bbb44815635a3
Erin-mp4 <erinobrien522@gmail.com>
2025-04-17 14:34:55 -0500

    Game Logic
    Coded in the game logic for rock paper scissors.

--- js/logic.js
+++ js/logic.js
@@ -1,9 +1,63 @@
 function rockPick() {
-    
+    function genPick() {
+        let pick = "img/mystery.svg";
+        if (Math.floor(Math.random() * 3) == 2) {
+            pick = "img/rock.svg";
+        } else if (Math.floor(Math.random() * 3) == 1) {
+            pick = "img/paper.svg";
+        } else {
+            pick = "img/scissors.svg";
+        }
+        document.getElementById('oppResult').src = pick;
+        if (pick == "img/rock.svg") {
+            document.getElementById('result').innerHTML = "It's a Tie :/";
+        } else if (pick == "img/paper.svg") {
+            document.getElementById('result').innerHTML = "You Lose :(";
+        } else {
+            document.getElementById('result').innerHTML = "You Win :)";
+        }
+    }
+    genPick();
 }
 function paperPick() {
-
+    function genPick() {
+        let pick = "img/mystery.svg";
+        if (Math.floor(Math.random() * 3) == 2) {
+            pick = "img/rock.svg";
+        } else if (Math.floor(Math.random() * 3) == 1) {
+            pick = "img/paper.svg";
+        } else {
+            pick = "img/scissors.svg";
+        }
+        document.getElementById('oppResult').src = pick;
+        if (pick == "img/paper.svg") {
+            document.getElementById('result').innerHTML = "It's a Tie :/";
+        } else if (pick == "img/scissors.svg") {
+            document.getElementById('result').innerHTML = "You Lose :(";
+        } else {
+            document.getElementById('result').innerHTML = "You Win :)";
+        }
+    }
+    genPick();
 }
 function scissorsPick() {
-    
+    function genPick() {
+        let pick = "img/mystery.svg";
+        if (Math.floor(Math.random() * 3) == 2) {
+            pick = "img/rock.svg";
+        } else if (Math.floor(Math.random() * 3) == 1) {
+            pick = "img/paper.svg";
+        } else {
+            pick = "img/scissors.svg";
+        }
+        document.getElementById('oppResult').src = pick;
+        if (pick == "img/scissors.svg") {
+            document.getElementById('result').innerHTML = "It's a Tie :/";
+        } else if (pick == "img/rock.svg") {
+            document.getElementById('result').innerHTML = "You Lose :(";
+        } else {
+            document.getElementById('result').innerHTML = "You Win :)";
+        }
+    }
+    genPick();    
 }
